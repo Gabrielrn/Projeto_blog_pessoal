59d9ab8b637fbb77a80fdef4279c296b11b0c272
Gabriel Rodrigues do Nascimento <gabriel.nascimentordn@gmail.com>
2022-09-16 10:51:23 -0300

    Teste JUnit

--- blogpessoal/src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
+++ blogpessoal/src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
@@ -25,15 +25,19 @@ import com.generation.blogpessoal.service.UsuarioService;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UsuarioControllerTest {
 	
+	//serve para ter acesso aos verbos http(get post put delete) em modo teste
 	@Autowired
 	private TestRestTemplate testRestTemplate;
 	
+	// serve para conseguirmos usar as funções de serviço do usuario
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	//serve para acesso ao banco de dados h2
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	//antes de começar o teste, limpa o banco de dados h2 e cadastra um novo usuário
 	@BeforeAll
 	void start() {
 		
@@ -43,18 +47,25 @@ public class UsuarioControllerTest {
 				"Root", "root@root.com", "rootroot", " "));
 	}
 	
+	//indica que o código abaixo será um teste
 	@Test
+	// indica um nome de exibição para o teste no console do JUnit
 	@DisplayName("Cadastrar Um Usuário")
 	public void deveCriarUmUsuario() {
 		
+		// define o que eu estou mandando para minha api enviar para o banco de dados
 		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario(0L,
 				"Paulo Antunes", "paulo_antunes@email.com.br", "13465278", "https://i.imgur.com/yKSBW2a_d.webp?maxwidth=520&shape=thumb&fidelity=high"));
-			
+		
+		// define o que eu vou obter como resposta do que foi 'persistido' no banco de dados 'h2'
 		ResponseEntity<Usuario> corpoResposta = testRestTemplate
 				.exchange("/usuarios/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
 		
+		// faço a verificação se o status http da resposta foi igual a 201 Created
 		assertEquals(HttpStatus.CREATED, corpoResposta.getStatusCode());
+		// faço a verificação se o que eu mandei de nome do usuario foi o que efetivamente chegou no banco de dados
 		assertEquals(corpoRequisicao.getBody().getNome(), corpoResposta.getBody().getNome());
+		// faço a verificação se o que eu mandei de email do usuario foi o que efetivamente chegou no banco de dados
 		assertEquals(corpoRequisicao.getBody().getUsuario(), corpoResposta.getBody().getUsuario());
 		
 	}
@@ -78,26 +89,26 @@ public class UsuarioControllerTest {
 	@Test
 	@DisplayName("Atualizar um Usuário")
 	public void deveAtualizarUmUsuario() {
-		
-		Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(new Usuario(0L,
-				"Juliana Andrews", "juliana_andrews@email.com.br", "juliana123", "https://i.imgur.com/mMCRK0m_d.webp?maxwidth=520&shape=thumb&fidelity=high"));
-		
-		Usuario usuarioUpdate = new Usuario(usuarioCadastrado.get().getId(),
-				"Jualiana Andrews Ramos", "juliana_ramos@email.com.br", "juliana123", "https://i.imgur.com/mMCRK0m_d.webp?maxwidth=520&shape=thumb&fidelity=high");
+
+		Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(new Usuario(0L, 
+			"Juliana Andrews", "juliana_andrews@email.com.br", "juliana123", "https://i.imgur.com/yDRVeK7.jpg"));
+
+		Usuario usuarioUpdate = new Usuario(usuarioCadastrado.get().getId(), 
+			"Juliana Andrews Ramos", "juliana_ramos@email.com.br", "juliana123" , "https://i.imgur.com/yDRVeK7.jpg");
 		
 		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuarioUpdate);
-		
+
 		ResponseEntity<Usuario> corpoResposta = testRestTemplate
-				.withBasicAuth("@root@root.com", "rootroot")
-				.exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
-		
+			.withBasicAuth("root@root.com", "rootroot")
+			.exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
+
 		assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
 		assertEquals(corpoRequisicao.getBody().getNome(), corpoResposta.getBody().getNome());
 		assertEquals(corpoRequisicao.getBody().getUsuario(), corpoResposta.getBody().getUsuario());
 	}
 	
 	@Test
-	@DisplayName("Atualizar um Usuário")
+	@DisplayName("Listar todos os Usuários")
 	public void deveMostrarTodosUsuarios() {
 		
 		usuarioService.cadastrarUsuario(new Usuario(0L,
--- blogpessoal/src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
+++ blogpessoal/src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
@@ -17,18 +17,22 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import com.generation.blogpessoal.model.Usuario;
 
+//indicica que a classe UsuarioRepositoryTest é uma classe de test, e que esse teste será rodado em uma porta aleatória local no meu computador (desde que ela não esteja sendo usada)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//indica que o teste a ser feito vai ser um teste unitário por classe
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UsuarioRepositoryTest {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	// inserindo usuarios no meu banco de dados h2, para que eu possa testar as funções de procurar um usuario por nome e por email
 	@BeforeAll
 	void start() {
 		
 		usuarioRepository.deleteAll();
 		
+		// inserindo usuarios no meu banco de dados h2, para que eu possa testar as funções de procurar um usuario por nome e por email
 		usuarioRepository.save(new Usuario(0L, "João da Silva", "joao@email.com", "13465278", "https://i.imgur.com/cuQdfFe_d.webp?maxwidth=520&shape=thumb&fidelity=high"));
 		
 		usuarioRepository.save(new Usuario(0L, "Manuela da Silva", "manuela@email.com", "13465278", "https://i.imgur.com/KhswlY9_d.webp?maxwidth=520&shape=thumb&fidelity=high"));
@@ -38,11 +42,15 @@ public class UsuarioRepositoryTest {
 		usuarioRepository.save(new Usuario(0L, "Paulo Antunes", "paulo@email.com", "13465278", "https://i.imgur.com/yKSBW2a_d.webp?maxwidth=520&shape=thumb&fidelity=high"));
 	}
 	
+	// indica o inicio do test
 	@Test
+	// indica o nome do test
 	@DisplayName("Retorna 1 usuário")
 	public void deveRetornarUmUsuario() {
 		
+		// o que eu espero receber da api, buscando um usuario pelo seu email
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario("joao@email.com");
+		// comparando se o que eu esperava receber, foi o que de fato meu teste trouxe
 		assertTrue(usuario.get().getUsuario().equals("joao@email.com"));
 	}
 	
