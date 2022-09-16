60afcbf9b00a1a50cb0cceb6d6a99e460506b1bb
Gabriel Rodrigues do Nascimento <gabriel.nascimentordn@gmail.com>
2022-09-16 12:31:19 -0300

    Deploy do Projeto Blog Pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -26,7 +28,8 @@ public class Usuario {
 
     @NotNull(message = "O Atributo Nome é Obrigatório!")
     private String nome;
-
+    
+    @Schema(example = "email@email.com.br")
     @NotNull(message = "O Atributo Usuário é Obrigatório!")
     @Email(message = "O Atributo Usuário deve ser um email válido!")
     private String usuario;
@@ -42,8 +45,6 @@ public class Usuario {
     @JsonIgnoreProperties("usuario")
     private List<Postagem> postagem;
     
-    
-
 	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
 		this.id = id;
 		this.nome = nome;
